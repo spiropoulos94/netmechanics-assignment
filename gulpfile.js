19f3578eb9b1aacaa2b563f6ae3d7480ac1d318a
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-07-22 16:06:18 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,11 @@ const sass = require("gulp-sass");
 
 const browserSync = require("browser-sync").create();
 
-// gulp.task("copyHtml", async function() {
-//   gulp.src("src/*.html").pipe(gulp.dest("dist"));
-// });
-//
-// gulp.task("sass", async function() {
-//   return gulp
-//     .src("./src/sass/*.scss")
-//     .pipe(sass())
-//     .pipe(gulp.dest("./dist/css"));
-// });
-
-//compile scss into css
-
 function style() {
   return (
     gulp
       .src("./assets/scss/*.scss")
-      .pipe(sass())
+      .pipe(sass().on("error", sass.logError))
       .pipe(gulp.dest("./dist/assets/css"))
       //stream changes
       .pipe(browserSync.stream())
